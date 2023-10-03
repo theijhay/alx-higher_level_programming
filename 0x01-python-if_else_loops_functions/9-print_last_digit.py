3e849f98066d7113da3913d0101a3800e126a778
theijhay <olawaleisaacjohn@gmail.com>
2023-10-03 15:36:12 -0700

    Write a function that prints a string in uppercase followed by a new line.

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+# 9-print_last_digit.py
+
+
 def print_last_digit(number):
-    print(abs(number) % 10, end='')
-    return(abs(number) % 10)
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
