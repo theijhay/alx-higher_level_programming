13e99668fec72dc9d593b31a59703d6cfe0c36e3
theijhay <olawaleisaacjohn@gmail.com>
2023-10-03 13:31:00 -0700

    Print all the letters except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-
-    # prints the ASCII alphabet
     if chr(letter) is not 'q' and chr(letter) is not 'e':
         print("{}".format(chr(letter)), end="")
