d3601962fcf81e59c78b98627b6a97ae83928f31
theijhay <olawaleisaacjohn@gmail.com>
2023-10-23 18:21:52 -0700

    the Python function

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    # Assign the value to the integer.
+    result = 0
+    # returns a sequence of numbers from 0 to the specified length.
+    for i in range(1, 3):
+        try:
+            # Check which one is greater or lesser.
+            if i > a:
+                # Raise the exception not too far.
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        try:
+            result = b + a
+            break
+    return (result)
