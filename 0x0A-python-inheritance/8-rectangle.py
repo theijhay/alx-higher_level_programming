a69aba9e47d809adc53929915d873dbc8bb89c51
theijhay <olawaleisaacjohn@gmail.com>
2023-11-06 20:22:01 -0800

    a class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" Class that defines a rectangle from BaseGeometry Class """
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Class that defines a rectangle from BaseGeometry Class """
+    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """ Initializes instance """
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-    def area(self):
-        """ Method that returns the area of the instance"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ Special method that returns the printable string """
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
