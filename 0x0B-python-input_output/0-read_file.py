8c200e76d9230c745640837cd27a03199aca9230
theijhay <olawaleisaacjohn@gmail.com>
2023-11-07 13:13:53 -0800

    function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file
+Contains function that reads and prints contents from file
+"""
 
-"""Defines an object attribute lookup function."""
 
-
-def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+def read_file(filename=""):
+    """Read and print text from file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
