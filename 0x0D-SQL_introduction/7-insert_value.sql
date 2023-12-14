96101f0462a9485b2a271943b2b06acf4e9595bf
theijhay <olawaleisaacjohn@gmail.com>
2023-12-14 23:06:55 +0100

    inserts a new row in the table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- script that displays the number of records with id = 89 in the table first_table
 SELECT COUNT(*) FROM first_table WHERE id = '89'
