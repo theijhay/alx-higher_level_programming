5b38e060822bffc3fa966797b8286ce19e8f5c35
theijhay <olawaleisaacjohn@gmail.com>
2023-12-14 23:04:09 +0100

    inserts a new row in the table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
-SELECT COUNT(*) FROM irst_table WHERE id= '89'
+SELECT COUNT(*) FROM first_table WHERE id = 89
