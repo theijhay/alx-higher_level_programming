45f97f48e9dbc0c6b7cc28bafdcdb40c214a126a
theijhay <olawaleisaacjohn@gmail.com>
2023-12-14 21:46:13 +0100

    show tables

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- creates a table called first_table in the current database in your MySQL server
-CREATE TABLES IF NOT EXISTS first_table
+CREATE TABLE IF NOT EXISTS first_table
 id INT
 name VARCHAR(256)
