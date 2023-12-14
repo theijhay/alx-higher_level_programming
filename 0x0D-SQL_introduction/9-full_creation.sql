ccf085e937bf7a4cfb16f19df847313c88b9b374
theijhay <olawaleisaacjohn@gmail.com>
2023-12-15 00:32:55 +0100

    creates a table second_table in the database hbtn_0c_0

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- create a table second_table in hbtn_0c_0
 -- add multiple rows
-CREATE TABLE IF NOT EXISTS second_table (id INT
-name VARCHAR(256)
-score INT)
+CREATE TABLE IF NOT EXISTS second_table (id INT.
+name VARCHAR(256),
+score INT);
 
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10)
 INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3)
