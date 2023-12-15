b2c3817f770d9c04f256bb0ab4a593c581ebcc51
theijhay <olawaleisaacjohn@gmail.com>
2023-12-15 01:43:07 +0100

    lists records of the table second_table

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,4 +2,4 @@
 -- from the database hbtn_0c_0
 -- results display score and name
 -- ordered by score
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC
