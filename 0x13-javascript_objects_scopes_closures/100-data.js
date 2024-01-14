e394c4b669c38ca0bf42098796da5b88e46651dd
theijhay <olawaleisaacjohn@gmail.com>
2024-01-14 15:25:03 +0100

    Write a script that concats 2 files

---
+++ 0x13-javascript_objects_scopes_closures/100-data.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// script that imports an array and computes a new array.
+exports.list = [1, 2, 3, 4, 5];
---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// script that imports an array and computes a new array.
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
