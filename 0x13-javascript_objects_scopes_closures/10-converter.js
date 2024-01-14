119e397b7a69d3d5740f6f060a133b6e26969a2a
theijhay <olawaleisaacjohn@gmail.com>
2024-01-14 14:54:56 +0100

    function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// function that converts a number from base 10 to another base passed as argument.
+exports.converter = function (base) { return num => num.toString(base); };
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// function that prints the number of arguments already printed and the new argument value.
+let count = 0;
+exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
